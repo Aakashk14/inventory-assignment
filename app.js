fc01c3803dc6dd5dddbcac4d67931df7da9b9a70
Aakash Kumar <64356169+Aakashk14@users.noreply.github.com>
2022-06-22 04:21:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ mongoose.connect("mongodb://127.0.0.1:27017/inventory")
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
-app.listen(5000,"0.0.0.0")
 
 app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin","http://localhost:3000")
@@ -14,4 +13,5 @@ app.use(function(req,res,next){
     next()
 })
 
-app.use(require("./Main/inventory"))
+app.use(require("./Main/inventory")) //backend
+app.listen(5000,"0.0.0.0")
